d19db40c4e53ad6764e0ef39847ab27b20ed0d31
Carlos <A01741085@itesm.mx>
2020-10-29 11:40:55 -0700

    Cambio de nombre en funciones

--- K-means.py
+++ K-means.py
@@ -13,29 +13,49 @@ def genData(n,seed=1,m=False): #Genera data linearmente separable por una pendie
     X = (X-0.5)*2
     return [X,y]
 
-def graphPoints(X,y): #Grafica los puntos y los puntos proyectados
-    for i in range(0,X.shape[0]):
-        p = X[i,0:2]
-        #p2 = Xp[i,0:2]            
-        if(y[i]==0):
-            plt.scatter(p[0],p[1],color="red", s=4)
-        elif(y[i]==1):
-            plt.scatter(p[0],p[1],color="blue", s=4)
-        elif(y[i]==2):
-            plt.scatter(p[0],p[1],color="green", s=4)
-        elif(y[i]==3):
-            plt.scatter(p[0],p[1],color="brown", s=4)
-        elif(y[i]==4):
-            plt.scatter(p[0],p[1],color="grey", s=4)
-        elif(y[i]==5):
-            plt.scatter(p[0],p[1],color="gold", s=4)
-        else:
-            plt.scatter(p[0],p[1],color="black", s=4)
-    plt.xlim(-1, 1)
-    plt.ylim(-1, 1)
-    plt.show()
+def graphPoints(X,y,centroide=True): #Grafica los puntos y los puntos proyectados
+    if(centroide):
+        for i in range(0,X.shape[0]):
+            p = X[i,0:2]
+            #p2 = Xp[i,0:2]            
+            if(y[i]==0):
+                plt.scatter(p[0],p[1],color="red", s=4)
+            elif(y[i]==1):
+                plt.scatter(p[0],p[1],color="blue", s=4)
+            elif(y[i]==2):
+                plt.scatter(p[0],p[1],color="green", s=4)
+            elif(y[i]==3):
+                plt.scatter(p[0],p[1],color="brown", s=4)
+            elif(y[i]==4):
+                plt.scatter(p[0],p[1],color="grey", s=4)
+            elif(y[i]==5):
+                plt.scatter(p[0],p[1],color="gold", s=4)
+            else:
+                plt.scatter(p[0],p[1],color="black", s=4)
+            
+    else:
+        for i in range(0,X.shape[0]):
+            p = X[i,0:2]
+            #p2 = Xp[i,0:2]            
+            if(y[i]==0):
+                plt.scatter(p[0],p[1],color="red",marker='X', s=4)
+            elif(y[i]==1):
+                plt.scatter(p[0],p[1],color="blue",marker='X', s=4)
+            elif(y[i]==2):
+                plt.scatter(p[0],p[1],color="green",marker='X', s=4)
+            elif(y[i]==3):
+                plt.scatter(p[0],p[1],color="brown",marker='X', s=4)
+            elif(y[i]==4):
+                plt.scatter(p[0],p[1],color="grey",marker='X', s=4)
+            elif(y[i]==5):
+                plt.scatter(p[0],p[1],color="gold",marker='X', s=4)
+            else:
+                plt.scatter(p[0],p[1],color="black",marker='X', s=4)
+        plt.xlim(-1, 1)
+        plt.ylim(-1, 1)
+        
 
-def createXClases(n,seed=2,m=False): # Crear dos tipos de puntos para clasificar
+def crearCentroides(n,seed=2,m=False): # Crear tipos de puntos para clasificar
     np.random.seed(seed)
     if(m==False):
     	m= ((rr(20)+1)/10)**0.5
@@ -45,7 +65,7 @@ def createXClases(n,seed=2,m=False): # Crear dos tipos de puntos para clasificar
     y = [0,1,2,3,4,5]
     return [X,y]
 
-def distanceX1AndX2(X2,X): # Crear matriz con la distancia de cada punto al punto clasificador
+def distancias(X2,X): # Crear matriz con la distancia de cada punto al punto clasificador
     listDistance = np.zeros((len(X),len(X2)))
     for i in range(len(X2)):
         for j in range(len(X)):
@@ -53,7 +73,7 @@ def distanceX1AndX2(X2,X): # Crear matriz con la distancia de cada punto al punt
             listDistance[j][i] = distance
     return listDistance
 
-def distanceX1OrX2(yy,X,listDistanceX1AndX2): # Crear matriz con el punto clasificador mas cercano
+def clasificador(yy,X,listDistanceX1AndX2): # Crear matriz con el punto clasificador mas cercano
     listClasification = np.zeros(len(X))
     for j in range(0,len(X)):
         listClasification[j] = posMayor(listDistanceX1AndX2[j],0,0,0)
@@ -68,17 +88,20 @@ def posMayor(listDistanceX1AndX2,j,i,posi): # Regresa el tipo de punto clasifica
          return posMayor(listDistanceX1AndX2,j+1,i,posi)
     else:
         return posi
-       
+    
 
 [X,y] = genData(200)
 graphPoints(X,y)
 
-[X2,yy] = createXClases(3)
-graphPoints(X2,yy)
+[X2,yy] = crearCentroides(3)
+graphPoints(X2,yy,False)
 
-listDistanceX1AndX2 = distanceX1AndX2(X2,X)
+listDistance = distancias(X2,X)
 
-distanceX1OrX2 = distanceX1OrX2(X2,X,listDistanceX1AndX2)
+listClasificador = clasificador(X2,X,listDistance)
 
-graphPoints(X,distanceX1OrX2)
+graphPoints(X,listClasificador)
+plt.show()
+    
+
 
