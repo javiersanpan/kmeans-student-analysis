b3ab6fb085364ed1fd8d45dfc7fb922147262da4
Carlos <Carlosnoel_ojedaa@icloud.com>
2020-10-28 22:49:49 -0700

    Crear hasta 5 grupos en 2D

--- K-means.py
+++ K-means.py
@@ -9,7 +9,7 @@ def genData(n,seed=1,m=False): #Genera data linearmente separable por una pendie
     if(m==False):
     	m= (rr(20)+1)/10
     X = np.random.random((n, 2))
-    y = ((X[:,1]/X[:,0])>m)*1
+    y = ((X[:,1]/X[:,0])>m)*1+20
     X = (X-0.5)*2
     return [X,y]
 
@@ -17,8 +17,18 @@ def graphPoints(X,y): #Grafica los puntos y los puntos proyectados
     for i in range(0,X.shape[0]):
         p = X[i,0:2]
         #p2 = Xp[i,0:2]            
-        if(y[i]):
+        if(y[i]==0):
             plt.scatter(p[0],p[1],color="red", s=4)
+        elif(y[i]==1):
+            plt.scatter(p[0],p[1],color="blue", s=4)
+        elif(y[i]==2):
+            plt.scatter(p[0],p[1],color="green", s=4)
+        elif(y[i]==3):
+            plt.scatter(p[0],p[1],color="brown", s=4)
+        elif(y[i]==4):
+            plt.scatter(p[0],p[1],color="grey", s=4)
+        elif(y[i]==5):
+            plt.scatter(p[0],p[1],color="gold", s=4)
         else:
             plt.scatter(p[0],p[1],color="black", s=4)
     plt.xlim(-1, 1)
@@ -31,6 +41,7 @@ def createXClases(n,seed=1,m=False): #Modificar para crear dos tipos de puntos
     X = np.random.random((n, 2))
     y = ((X[:,1]/X[:,0])>m)*1
     X = (X-0.5)*2
+    y = [0,1,2,3,4,5]
     return [X,y]
 
 def distanceX1AndX2(X2,X):
@@ -41,27 +52,32 @@ def distanceX1AndX2(X2,X):
             listDistance[j][i] = distance
     return listDistance
 
-def distanceX1OrX2(yy,X,listdistanceX1AndX2):
-    i = 0
+def distanceX1OrX2(yy,X,listDistanceX1AndX2):
     listClasification = np.zeros(len(X))
     for j in range(0,len(X)):
-        if(listdistanceX1AndX2[j][i]>listdistanceX1AndX2[j][i+1]):
-            #listdistanceX1AndX2[j][i+1] = 0
-            listClasification[j] = 1  #Modificar de acuerdo al tipo de punto
-        else:
-            #listdistanceX1AndX2[j][i+1] = 1
-            listClasification[j] = 0  #Modificar de acuerdo al tipo de punto
+        listClasification[j] = posMayor(listDistanceX1AndX2[j],0,0,0)
     return listClasification
 
-[X,y] = genData(20)
+def posMayor(listDistanceX1AndX2,j,i,posi):
+    if (j == 0):
+        return posMayor(listDistanceX1AndX2,j+1,listDistanceX1AndX2[0],0)
+    elif ((j < len(listDistanceX1AndX2)) and (i > listDistanceX1AndX2[j])):
+        return posMayor(listDistanceX1AndX2,j+1,listDistanceX1AndX2[j],j)
+    elif (j < len(listDistanceX1AndX2)):
+         return posMayor(listDistanceX1AndX2,j+1,i,posi)
+    else:
+        return posi
+       
+
+[X,y] = genData(200)
 graphPoints(X,y)
 
-[X2,yy] = createXClases(2)
+[X2,yy] = createXClases(3)
 graphPoints(X2,yy)
 
-listdistanceX1AndX2 = distanceX1AndX2(X2,X)
+listDistanceX1AndX2 = distanceX1AndX2(X2,X)
 
-distanceX1OrX2 = distanceX1OrX2(X2,X,listdistanceX1AndX2)
+distanceX1OrX2 = distanceX1OrX2(X2,X,listDistanceX1AndX2)
 
 graphPoints(X,distanceX1OrX2)
 
