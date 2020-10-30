d695ce3dd54f56290e53e0d98630021d4e9d01fb
SauceRSC <rodrisc2000@hotmail.com>
2020-10-29 20:31:30 -0600

    José Rodrigo Saucedo Cruz
    Integracion de la lectura de la base de datos, obtener el numero de clases y hacer el KMeans

--- K-means.py
+++ K-means.py
@@ -1,34 +1,79 @@
 import numpy as np
-import numpy.linalg as la
 import matplotlib.pyplot as plt
 from random import randrange as rr
-import random
-Import pandas as pd
+import pandas as pd
+from sklearn.manifold import Isomap
+from sklearn.manifold import MDS
+from sklearn.decomposition import PCA
+from sklearn.cluster import KMeans
+from sklearn.metrics import silhouette_samples, silhouette_score
 
-df = pd.read.csv("data.csv")
-data = df.values
-X = data[:,0:-1]
-n = len(X)
-Y = np.zeros(n)+50
+#Se hace la reduccion de 7D a 2D
+def reduccion(X):
 
-def genData(n,seed=1,m=False): #Genera data linearmente separable por una pendiente (m)
-    np.random.seed(seed)
-    if(m==False):
-    	m= (rr(20)+1)/10
-    X = np.random.random((n, 2))
-    y = np.zeros((n))+50
-    X = (X-0.5)*2
-    return [X,y]
+    pca = PCA(n_components=2)
+    X1t = pca.fit_transform(X)
+    
+    #Por alguna razon cambia en cada iteracion, por lo que no lo consideramos confiable
+    emb = MDS(n_components=2)
+    X2t = emb.fit_transform(X)
+    
+    emb = Isomap(n_components=2)
+    X3t = emb.fit_transform(X)
+    
+    plt.scatter(X1t[:,0],X1t[:,1])
+    plt.title('Alumnos dataset, PCA ')
+    plt.show()
+    plt.scatter(X2t[:,0],X2t[:,1])
+    plt.title('Alumnos dataset, MDS')
+    plt.show()
+    plt.scatter(X3t[:,0],X3t[:,1])
+    plt.title('Alumnos dataset, Isomap ')
+    plt.show()
+    
+    return X3t
 
-def graphPoints(X,y,centroide=True): #Grafica los puntos y los puntos proyectados
+#Se determina K, el numero de clases, mediante Silhouette
+def silhouette(X):
+    k = 0
+    silhouette_avg_past = 0
+    range_n_clusters = range(2, 10)
+
+    for n_clusters in range_n_clusters:
+        
+        clusterer = KMeans(n_clusters=n_clusters, random_state=10)
+        cluster_labels = clusterer.fit_predict(X)
+        silhouette_avg = silhouette_score(X, cluster_labels)
+        if(silhouette_avg_past < silhouette_avg):
+            k=n_clusters
+            silhouette_avg_past = silhouette_avg
+        
+        #Para imprimir el score para cada K y ver cual es el mas alto
+        #print("For n_clusters =", n_clusters,"The average silhouette_score is :", silhouette_avg)
+        sample_silhouette_values = silhouette_samples(X, cluster_labels)
+    
+    return k
+
+#Se determina K, el numero de clases,de forma visual
+def obtenerK(X):
+    Nc = range(2, 10)
+    kmeans = [KMeans(n_clusters=i) for i in Nc]
+    kmeans
+    score = [kmeans[i].fit(X).score(X) for i in range(len(kmeans))]
+    score
+    plt.plot(Nc,score)
+    plt.xlim(0, 10)
+    plt.show()
+
+#Grafica los puntos y los puntos proyectados
+def graphPoints(X,y,centroide=True): 
     if(centroide):
         plt.scatter(X[:,0],X[:,1],c=y)
     else:
         plt.scatter(X[:,0],X[:,1],c=y,marker='x', s=200)
-    plt.xlim(-1, 1)
-    plt.ylim(-1, 1)
 
-def crearCentroides(n,seed=2,m=False): # Crear tipos de puntos para clasificar
+# Crear centroides aleatorios para despues reubicarlos
+def crearCentroides(n,seed=2,m=False): 
     np.random.seed(seed)
     if(m==False):
     	m= ((rr(20)+1)/10)**0.5
@@ -37,7 +82,8 @@ def crearCentroides(n,seed=2,m=False): # Crear tipos de puntos para clasificar
     y = np.arange(n)
     return [X,y]
 
-def distancias(X2,X): # Crear matriz con la distancia de cada punto al punto clasificador
+# Crear matriz con la distancia de cada punto al punto clasificador
+def distancias(X2,X): 
     listDistance = np.zeros((len(X),len(X2)))
     for i in range(len(X2)):
         for j in range(len(X)):
@@ -45,13 +91,15 @@ def distancias(X2,X): # Crear matriz con la distancia de cada punto al punto cla
             listDistance[j][i] = distance
     return listDistance
 
-def clasificador(yy,X,listDistanceX1AndX2): # Crear matriz con el tipo de punto clasificador mas cercano
+# Crear matriz con el tipo de punto clasificador mas cercano
+def clasificador(yy,X,listDistanceX1AndX2): 
     listClasification = np.zeros(len(X))
     for j in range(0,len(X)):
         listClasification[j] = posMenor(listDistanceX1AndX2[j],0,0,0)
     return listClasification
 
-def posMenor(listDistanceX1AndX2,j,i,posi): # Regresa el tipo de punto al que pertenece dicho punto
+# Regresa la clase a la que pertenece dicho punto
+def posMenor(listDistanceX1AndX2,j,i,posi): 
     if (j == 0):
         return posMenor(listDistanceX1AndX2,j+1,listDistanceX1AndX2[0],0)
     elif ((j < len(listDistanceX1AndX2)) and (i > listDistanceX1AndX2[j])):
@@ -60,15 +108,17 @@ def posMenor(listDistanceX1AndX2,j,i,posi): # Regresa el tipo de punto al que pe
          return posMenor(listDistanceX1AndX2,j+1,i,posi)
     else:
         return posi
-    
-def centroides(X,listClasificador,yy): # Regresa los centroides con sus respectivas coordenadas
+
+# Regresa los nuevos centroides con sus respectivas coordenadas
+def centroides(X,listClasificador,yy): 
     XX = np.zeros((len(yy),2))
     for i in range(len(yy)):
         XX[i] = (sum(X[np.where(listClasificador==i)])/len(np.where(listClasificador==i)[0]))
     return XX
 
-def iniciar():
-    [X2,yy] = crearCentroides(5)
+# Hace el Kmeans para 2D
+def KMeans2D(k):
+    [X2,yy] = crearCentroides(k)
     graphPoints(X2,yy,False)
     
     listDistance = distancias(X2,X)
@@ -88,8 +138,15 @@ def iniciar():
         
         if (np.linalg.norm(X2Viejo) == np.linalg.norm(X2)):
             run = False
-            
-[X,y] = genData(200)
-graphPoints(X,y)
-plt.show()
-iniciar()
+
+
+df = pd.read_csv("data.csv")
+data = df.values
+X = data[:,0:]
+y = np.zeros((len(X)))
+
+X=reduccion(X)
+obtenerK(X)
+K= silhouette(X)
+#print("K = ",K) 
+KMeans2D(K)
